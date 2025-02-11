08aca7739e8b04778f71fefaa7ce544be5833fec
6rkdtui <dima.kazaeff@gmail.com>
2025-02-11 09:11:41 +0300

    исправленное
    вынес курсы валют в константы

--- 7-converter/dz.js
+++ 7-converter/dz.js
@@ -1,24 +1,31 @@
 function converter (sum, sourceCurrency, targetCurrency){
     const key = `${sourceCurrency}-${targetCurrency}`;
 
+    const rubToDollar = 0.01;
+    const dolToRubble = 97.81;
+    const dolToEuro = 0.97;
+    const eueoToRDollar = 1.04;
+    const euroToRubble = 102.13;
+    const rubToEuro = 0.009791;
+
     switch (key){
         case 'руб-доллар':
-            console.log(`Примерно ${sum} рублей к долларам вернет ${0.01*sum} $`);
+            console.log(`Примерно ${sum} рублей к долларам вернет ${rubToDollar * sum} $`);
             break;
         case 'доллар-руб':
-            console.log(`Примерно ${sum} долларов к рублям вернет ${97.81*sum} руб`);
+            console.log(`Примерно ${sum} долларов к рублям вернет ${dolToRubble * sum} руб`);
             break;
         case 'доллар-евро':
-            console.log(`Примерно ${sum} долларов к евро вернет ${0.97*sum} евро`);
+            console.log(`Примерно ${sum} долларов к евро вернет ${dolToEuro * sum} евро`);
             break;
         case 'евро-доллар':
-            console.log(`Примерно ${sum} евро к долларам вернет ${1.04*sum} $`);
+            console.log(`Примерно ${sum} евро к долларам вернет ${eueoToRDollar * sum} $`);
             break;
         case 'евро-руб':
-            console.log(`Примерно ${sum} евро к рублям вернет ${102.13*sum} руб`);
+            console.log(`Примерно ${sum} евро к рублям вернет ${euroToRubble * sum} руб`);
             break;
         case 'руб-евро':
-            console.log(`Примерно ${sum} рублей к евро вернет ${0.009791*sum} евро`);
+            console.log(`Примерно ${sum} рублей к евро вернет ${rubToEuro * sum} евро`);
             break;
         default:
             console.log(null);
