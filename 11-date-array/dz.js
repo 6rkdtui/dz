026f2ce83558673381c30b981191a41acca23b59
6rkdtui <dima.kazaeff@gmail.com>
2025-02-17 11:28:26 +0300

    испраленное
    с маленькой буквы нужно называть функции, так же я бы вынес ее вне функции filter потому что иначе на каждый вызов она будет создаваться заново

--- 11-date-array/dz.js
+++ 11-date-array/dz.js
@@ -1,13 +1,14 @@
 const arrStrings = ['10-02-2022', 'тест', '11/12/2023', '00/13/2022', '41/12/2023'];
 
-function filterDate(arr){
 
-    function CheckDayAndWeek (day, month, year, arr){
-        if (day >= 1 && day <= 31 && month >= 1 && month <= 12) {
-            const convertedDate = day + '-' + month + '-' + year;
-            arr.push(convertedDate); 
-        }
+function checkDayAndWeek(day, month, year, arr){
+    if (day >= 1 && day <= 31 && month >= 1 && month <= 12) {
+        const convertedDate = day + '-' + month + '-' + year;
+        arr.push(convertedDate); 
     }
+}
+
+function filterDate(arr, fn){
 
     // массив, куда будем загонять валидные даты
     const validDates = []; 
@@ -19,17 +20,18 @@ function filterDate(arr){
             const month = Number(date.slice(3, 5));
             const year = Number(date.slice(6, 10));
 
-            CheckDayAndWeek(day, month, year, validDates);
+            fn(day, month, year, validDates);
 
         } else if (date.length === 10 && date[2] === '/' && date[5] === '/') {
             const day = Number(date.slice(3, 5));
             const month = Number(date.slice(0, 2));
             const year = Number(date.slice(6, 10));
 
-            CheckDayAndWeek(day, month, year, validDates);
+            fn(day, month, year, validDates);
         }     
     }
     )
     return validDates
 }
-console.log(filterDate(arrStrings));
+
+console.log(filterDate(arrStrings, checkDayAndWeek));
